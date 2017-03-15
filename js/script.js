29d44df9d228d947d039f8f31bb562e356c708c2
RatiboR1978 <klov29@mail.ru>
2017-03-15 11:07:41 +0700

    Перебросил сайт

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Mama on 22.09.2016.
+ */
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
