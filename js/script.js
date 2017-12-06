9ad4033cfba6062f354e3ccb764ca846cad57412
RatiboR1978 <klov29@mail.ru>
2017-12-07 06:31:25 +0700

    Прицепил яндекс карту и сделал мелкие правки

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+ymaps.ready(init);
+var myMap;
+
+function init(){
+  myMap = new ymaps.Map("map", {
+    center: [59.93866875, 30.32303304],
+    zoom: 17
+  });
+
+  myPlacemark = new ymaps.Placemark([59.93866875, 30.32303304], {
+    hintContent: 'Pink',
+    balloonContent: 'Приложение №1'
+  },
+  {
+    preset: 'islands#redIcon'
+  });
+
+
+  myMap.controls.remove('geolocationControl');
+  myMap.controls.remove('routeButtonControl');
+  myMap.controls.remove('trafficControl');
+  myMap.controls.remove('typeSelector');
+  myMap.controls.remove('fullscreenControl');
+  myMap.controls.remove('rulerControl');
+  myMap.controls.remove('zoomControl');
+  myMap.controls.remove('searchControl');
+
+  myMap.geoObjects.add(myPlacemark);
+}
